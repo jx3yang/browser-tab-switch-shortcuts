07ab42d8de2488ba7c1043333b848c9604c173e5
Ji Xi Yang <jx3yang@uwaterloo.ca>
2025-01-13 23:09:38 -0800

    Implement Ctrl+Left/Right to switch between tabs and automatically uncollapse/collapse tag groups on enter/leave

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,1 +1,36 @@
-console.log('from background script')
+const updateGroupTabSafe = (groupId: number | undefined, updateProperties: chrome.tabGroups.UpdateProperties) => {
+    if (groupId !== undefined && groupId > 0) {
+        chrome.tabGroups.update(groupId, updateProperties);
+    }
+}
+
+const updateTabGroups = (currentTabIndex: number, newTabIndex: number, allTabs: chrome.tabs.Tab[]) => {
+    const currentGroupId = allTabs[currentTabIndex].groupId;
+    const newGroupId = allTabs[newTabIndex].groupId;
+
+    if (currentGroupId !== newGroupId) {
+        updateGroupTabSafe(currentGroupId, { collapsed: true });
+        updateGroupTabSafe(newGroupId, { collapsed: false });
+    }
+}
+
+const updateTab = (command: string, currentTabIndex: number, allTabs: chrome.tabs.Tab[]) => {
+    const cnt = allTabs.length;
+    const offset = (command === "switch_tab_left") ? -1 : 1;
+    const newIndex = (currentTabIndex + offset + cnt) % cnt;
+    const newTabId = allTabs[newIndex].id ?? currentTabIndex;
+    updateTabGroups(currentTabIndex, newIndex, allTabs);
+    chrome.tabs.update(newTabId, { active: true });
+}
+
+chrome.commands.onCommand.addListener((command: string) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (activeTabs: chrome.tabs.Tab[]) => {
+        if (activeTabs.length === 0) {
+            return;
+        }
+        const currentTabIndex = activeTabs[0].index;
+        chrome.tabs.query({ currentWindow: true }, (allTabs: chrome.tabs.Tab[]) => {
+            updateTab(command, currentTabIndex, allTabs);
+        });
+    });
+});
